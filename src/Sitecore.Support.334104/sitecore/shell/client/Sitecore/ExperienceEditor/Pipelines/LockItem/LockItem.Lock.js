464204fce8419e8ed48c46f612397c6d5f049e4c
DK\tpe <tpe@sitecore.net>
2019-07-02 17:37:25 +0300

    Fix issue by forcing page reload on Lock command

--- src/Sitecore.Support.334104/sitecore/shell/client/Sitecore/ExperienceEditor/Pipelines/LockItem/LockItem.Lock.js
+++ src/Sitecore.Support.334104/sitecore/shell/client/Sitecore/ExperienceEditor/Pipelines/LockItem/LockItem.Lock.js
@@ -5,11 +5,12 @@
       "/-/speak/v1/ExperienceEditor/ExperienceEditor.js",
     ], function (Sitecore, ExperienceEditor) {
         return ExperienceEditor.PipelinesUtil.generateRequestProcessor("ExperienceEditor.LockItem", function (response) {
-            
+    /*fix bug 334104 - always force page reload on item Lock
     if (response.context.currentContext.version != response.responseValue.value.Version) {
         ExperienceEditor.refreshOnItem(response.context.currentContext, true);
         }
-    
+    */
+     ExperienceEditor.refreshOnItem(response.context.currentContext, true);
 
     var locked = response.responseValue.value.Locked;
     ExperienceEditor.getPageEditingWindow().Sitecore.PageModes.ChromeManager.updateItemRevision(response.context.currentContext.itemId, response.responseValue.value.Revision);
